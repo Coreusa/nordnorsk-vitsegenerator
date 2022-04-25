0cfa43aab09d534174350c9e216fa1658776465b
Coreus <706874+Coreusa@users.noreply.github.com>
2022-04-25 17:40:05 +0200

    Add more of the text data onto JSON and add a rudimentary random sample output to test the word generator.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="container-fluid">
+    <MainNav />
     <div class="row">
       <router-view/>
     </div>
   </div>
 </template>
 
+<script>
+import { defineComponent } from 'vue'
+import MainNav from '@/components/MainNav.vue'
+export default defineComponent({
+  name: 'Index',
+  components: {
+    MainNav
+  }
+})
+</script>
 <style lang="less">
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,15 +1,87 @@
 <template>
-  <div>
-    Default
-  </div>
+<div>
+  <p>
+    {{ randomSentence }}
+  </p>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Sentences from '@/assets/sentences.json'
 export default {
   name: 'Home',
-  components: {
+  data () {
+    return {
+      sentences: Sentences
+    }
+  },
+  computed: {
+    introductions () {
+      return Sentences.introductions
+    },
+    persons () {
+      return Sentences.persons
+    },
+    backgrounds () {
+      return Sentences.backgrounds
+    },
+    places () {
+      return Sentences.places
+    },
+    settings () {
+      return Sentences.settings
+    },
+    locations () {
+      return Sentences.locations
+    },
+    verbs () {
+      return Sentences.verbs
+    },
+    foods () {
+      return Sentences.foods
+    },
+    middlePart () {
+      return Sentences.foods
+    },
+    randomSentence () {
+      const keys = [
+        'introductions',
+        'persons',
+        'backgrounds',
+        'places',
+        'settings',
+        'locations',
+        'verbs',
+        'foods',
+        'moods1',
+        'moods2'
+      ]
+      let sentence = ''
+      keys.map(e => {
+        sentence += this.shuffle(Sentences[e])[0]
+        if (e === 'foods') {
+          sentence += '. '
+        } else {
+          sentence += ' '
+        }
+      })
+      return sentence
+    }
+  },
+  methods: {
+    // Fisher Yates
+    shuffle (input) {
+      const a = input
+      const aLength = a.length
+      for (var i = aLength - 1; i >= 0; i--) {
+        var randomIndex = Math.floor(Math.random() * (i + 1))
+        var itemAtIndex = a[randomIndex]
+        a[randomIndex] = a[i]
+        a[i] = itemAtIndex
+      }
+      return a
+    }
   }
 }
 </script>
